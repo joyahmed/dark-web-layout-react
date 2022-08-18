7c6a00933899c0a277cc3427a8e5f1b367a37a79
joyahmed <joy_ahmed_007@yahoo.com>
2022-08-18 14:58:16 +0600

    ✅stat section completed

--- src/App.jsx
+++ src/App.jsx
@@ -20,11 +20,11 @@ const App = () => (
 			</div>
 		</div>
 
-    <div className={`bg-primary ${styles.flexStart}`}>
-      <div className={`${styles.boxWidth}`}>
-        <Hero />
-      </div>
-    </div>
+		<div className={`bg-primary ${styles.flexStart}`}>
+			<div className={`${styles.boxWidth}`}>
+				<Hero />
+			</div>
+		</div>
 
 		<div
 			className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}
